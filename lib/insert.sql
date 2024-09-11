a110bd0a1a93da28932c3973e30905132651ba14
Imelda Hope <imeldalarusso@gmail.com>
2024-09-11 19:40:42 +0300

    creates queries for db

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES("Mr. Chocolate", 10, "M", "brown", "calm", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES("Rowdy", 7, "M", "black", "aggressive", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES("Tabitha", 5, "F", "brown", "playful", false);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES("Sergeant Brown", 8, "M", "brown", "calm", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES("Melissa", 6, "F", "black", "curious", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES("Grinch", 12, "M", "white", "grumpy", false);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES("Wendy", 4, "F", "brown", "friendly", true);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES(NULL, 3, "F", "black", "shy", true);
